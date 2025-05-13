157fa39d412b31aa16ec56514113dbc3b4d77fa2
rubyLin <ruby.lin@urmart.com>
2025-05-13 13:51:49 +0800

    fix: modify eslint.config for tseslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,17 +4,53 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
-    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
-    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
-    tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
+export default tseslint.config(
+    // 全域設定
     {
+        ignores: ['dist', 'build', 'node_modules']
+    },
+
+    // 主設定
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            parser: tseslint.parser,
+            parserOptions: {
+                project: true
+            },
+            globals: globals.browser
+        },
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            react: pluginReact,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh
+        },
         rules: {
+            ...js.configs.recommended.rules,
+            ...tseslint.configs.recommended[0].rules,
+            ...pluginReact.configs.flat.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+
+            // 覆蓋規則
             'react/react-in-jsx-scope': 'off',
             'no-unused-vars': 'off',
-            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-            'no-undef': 'error'
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_'
+                }
+            ],
+            'no-undef': 'error',
+            'react-refresh/only-export-components': [
+                'warn',
+                {
+                    allowConstantExport: true
+                }
+            ]
         }
     }
-]);
+);
