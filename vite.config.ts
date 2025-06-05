de98150346f9acc5839711e939c058e97a59ae40
rubyLin <ruby0771134@gmail.com>
2025-06-06 01:41:03 +0800

    feat : add path alias and activityPage path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,16 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {}
+        }
+    },
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@hooks': path.resolve(__dirname, 'src/hooks'),
+            '@context': path.resolve(__dirname, 'src/context')
         }
     }
 });
