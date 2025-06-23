79cd7dbed373857cd515c0caeb5470e34780faec
quincy <kunchi68@gmail.com>
2025-06-23 18:22:41 +0800

    fix: remove redundant attribute

--- src/pages/organizer/manageActivity/ActivitySiteWithActivity.tsx
+++ src/pages/organizer/manageActivity/ActivitySiteWithActivity.tsx
@@ -96,7 +96,7 @@ const ActivitySiteWithActivity = (props: ShowTimeProps) => {
             {showTimeList.map(showTime => {
                 return (
                     <BorderWrapperShow key={showTime.id}>
-                        <Row className={`${isMobile ? 'flex-column' : ''}`} w>
+                        <Row className={`${isMobile ? 'flex-column' : ''}`}>
                             <Col className="first-col">
                                 <Row className="mb-2">
                                     <Col className="d-flex align-items-center">
