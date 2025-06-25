91d61939d8a8962490f9e1ece8506e5db9213bde
quincy <kunchi68@gmail.com>
2025-06-26 00:50:30 +0800

    feat: set upload file size limit
    - upload user avator limit: 5MB
    - upload activity image limit: 10MB

--- src/api/uploadApi.ts
+++ src/api/uploadApi.ts
@@ -2,16 +2,21 @@ import { UploadSchema } from '../schemas/uploadApi';
 import { ApiResponse } from '@type/ApiResponse';
 import { apiClient } from './client';
 
-export const uploadToS3 = async (userId: string, file: File, limit: boolean = true): Promise<string> => {
-    if (file.size > 2 * 1024 * 1024) {
-        throw new Error('File size exceeds 2MB limit');
+export const uploadToS3 = async (
+    userId: string,
+    file: File,
+    fileSizeLimit: number = 5,
+    isOrganizer: boolean = false
+): Promise<string> => {
+    if (file.size > fileSizeLimit * 1024 * 1024) {
+        throw new Error(`File size exceeds ${fileSizeLimit}MB limit`);
     }
 
     const formData = new FormData();
     formData.append('file', file);
     formData.append('user_id', userId);
 
-    const apiUrl = limit ? '/v1/user/upload' : '/v1/organizer/upload/image';
+    const apiUrl = isOrganizer ? '/v1/organizer/upload/image' : '/v1/user/upload';
     try {
         const response = await apiClient.post<ApiResponse<{ url: string }>>(apiUrl, formData, {
             headers: {
